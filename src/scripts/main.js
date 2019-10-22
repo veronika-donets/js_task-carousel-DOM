f4b446bb08dd96b4dec78034235eba251812d9ee
Veronika D <donets.veronika@yahoo.com>
2019-10-22 14:48:35 +0300

    modify js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const movePrev = document.querySelector('.carousel__btn_prev');
+const moveNext = document.querySelector('.carousel__btn_next');
 const slides = document.getElementById('thumbs');
 const dots = document.getElementsByClassName('carousel__dot');
+
 let position = 0;
 let currentDot = 0;
 
@@ -12,7 +15,22 @@ function changeDots(n) {
   dots[n].className += ' carousel__dot_active';
 }
 
-function moveNext() {
+movePrev.addEventListener('click', function() {
+  position -= 330;
+
+  if (position < 0) {
+    position = 990;
+    currentDot = 4;
+  }
+
+  slides.style.transition = 'right 1s';
+  slides.style.right = `${position}px`;
+
+  currentDot--;
+  changeDots(currentDot);
+});
+
+moveNext.addEventListener('click', function() {
   position += 330;
 
   if (position > 990) {
@@ -20,24 +38,9 @@ function moveNext() {
     currentDot = -1;
   }
 
+  // slides.style.transition = 'right 1s';
   slides.style.right = `${position}px`;
-  slides.style.transition = 'right 1s';
 
   currentDot++;
   changeDots(currentDot);
-}
-
-function movePrev() {
-  position -= 330;
-
-  if (position < 0) {
-    position = 990;
-    currentDot = 3;
-  }
-
-  slides.style.right = `${position}px`;
-  slides.style.transition = 'right 1s';
-
-  currentDot--;
-  changeDots(currentDot);
-}
+});
